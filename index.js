def14b0b1785c2b04641e704e1484e1a9d70df2f
Hasibul Islam <hasibulislam@HasibsMdioM2Max.fritz.box>
2024-06-01 12:43:20 +0200

    db connection  - done

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 require("dotenv").config();
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const port = process.env.PORT || 3004;
 
 // middlewares 
@@ -10,11 +11,29 @@ app.use(express.json());
 
 
 
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.qoryues.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+const dbConnect = async () => {
+    try {
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } catch (error) {
+        console.log(error);
+    }
+}
+dbConnect();
 
 
-
-
-
+// collections 
+const userCollection = client.db("assignment12").collection("users");
 
 
 
